388bf07fedfed03e78f76b6accfca1b85c3d8d4c
jayeeeesh <jayeshthakur132@gmail.com>
2026-05-12 11:37:32 -0400

    Connect to MongoDB and Implement the end point Connect Routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const giftRoutes = require('./routes/giftRoutes')
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
@@ -10,7 +11,7 @@ const {loadData} = require("./util/import-mongo/index");
 
 const app = express();
 app.use("*",cors());
-const port = 3060;
+const port = 3070;
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
@@ -23,7 +24,7 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes)
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
