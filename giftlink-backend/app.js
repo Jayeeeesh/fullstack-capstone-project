36d4d1ac61a8a950b1440b0f51c1ae17b59d4fde
Jayeeeesh <jayeshthakur132@gmail.com>
2026-05-18 11:58:40 -0400

    Develop Back-end Integration for Registration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-const giftRoutes = require('./routes/giftRoutes')
-const searchRoutes = require('./routes/searchRoutes');
+
 
 
 const connectToDatabase = require('./models/db');
@@ -25,11 +24,9 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-app.use('/api/gifts', giftRoutes)
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-app.use('/api/search', searchRoutes)
+const giftRoutes = require('./routes/giftRoutes')
+const searchRoutes = require('./routes/searchRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 
 const pinoHttp = require('pino-http');
@@ -39,10 +36,13 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes)
+
+app.use('/api/auth', authRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/search', searchRoutes)
+
 
 
 // Global Error Handler
