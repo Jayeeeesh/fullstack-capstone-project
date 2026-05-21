c90df9139ab1ee4830517e2b0fdd38e5b244e000
Jayeeeesh <jayeshthakur132@gmail.com>
2026-05-21 01:04:27 -0400

    Fix JSX comment errors

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -13,14 +13,14 @@ function DetailsPage() {
     useEffect(() => {
         const authenticationToken = sessionStorage.getItem('auth-token');
         if (!authenticationToken) {
-            // Task 1: Check for authentication and redirect
+            {/* Task 1: Check for authentication and redirect */}
             navigate('/app/login')
         }
 
         // get the gift to be rendered on the details page
         const fetchGift = async () => {
             try {
-                // Task 2: Fetch gift details
+               {/* Task 2: Fetch gift details */}
                 const response = await fetch(`${urlConfig.backendUrl}/api/gifts/${productId}`)
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
@@ -36,14 +36,14 @@ function DetailsPage() {
 
         fetchGift();
 
-        // Task 3: Scroll to top on component mount
+         {/* Task 3: Scroll to top on component mount */}
         window.scrollTo(0, 0);
 
     }, [productId, navigate]);
 
 
     const handleBackClick = () => {
-        // Task 4: Handle back click
+        {/* Task 4: Handle back click */}
         navigate(-1);
     };
 
@@ -86,13 +86,12 @@ function DetailsPage() {
                 <div className="card-body">
                     <div className="image-placeholder-large">
                         {gift.image ? (
-                            // Task 5: Display gift image
                             <img src={gift.image} alt={gift.name} className="product-image-large" />
                         ) : (
                             <div className="no-image-available-large">No Image Available</div>
                         )}
                     </div>
-                    // Task 6: Display gift details
+                    {/* Task 6: Display gift details */}
                     <p><strong>Category:</strong>
                     {gift.category}
                     </p>
@@ -112,7 +111,7 @@ function DetailsPage() {
             </div>
             <div className="comments-section mt-4">
                 <h3 className="mb-3">Comments</h3>
-				// Task 7: Render comments section by using the map function to go through all the comments
+				 {/* Task 7: Render comments section by using the map function */}
                 {comments.map((comment, index)  => (
                 <div key={index} className="card mb-3">
                     <div className="card-body">
