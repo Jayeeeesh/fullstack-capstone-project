8d032c7cd9dd07ecc4138131d43f670a54954c41
Jayeeeesh <jayeshthakur132@gmail.com>
2026-05-21 00:50:04 -0400

    Fix App.js build issues

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -12,8 +12,6 @@ import Profile from './components/Profile/Profile';
 
 
 function App() {
-    const navigate = useNavigate();
-
   return (
     <>
         <Navbar/>
